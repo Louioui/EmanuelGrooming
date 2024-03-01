a29ab785d592a45d3710bca805a2d906ff7d8dcb
luis pacheco <luispacheco13a@gmail.com>
2024-03-01 04:01:19 -0800

    fixing small adjustment to my respons capturing data from user input

--- Grooming/database/registration.sql
+++ Grooming/database/registration.sql
@@ -1,32 +1,40 @@
 -- Active: 1703403626338@@127.0.0.1@3306
-    
+
 CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100),
+    phone_number VARCHAR(15)
+);
+
 -- Create Dogs table
 CREATE TABLE Dogs (
-    dog_id INT AUTO_INCREMENT PRIMARY KEY,
+    dog_id INT PRIMARY KEY,
     customer_id INT,
     dog_name VARCHAR(50) NOT NULL,
     breed VARCHAR(50),
     age INT,
-    gender VARCHAR(4)
+    gender VARCHAR(4),
 );
 
 -- Create Services table
 CREATE TABLE Services (
-    service_id INT AUTO_INCREMENT PRIMARY KEY,
+    service_id INT PRIMARY KEY,
     service_name VARCHAR(50) NOT NULL,
     price DECIMAL(10, 2) NOT NULL
 );
 
 -- Create Appointments table
 CREATE TABLE Appointments (
-    appointment_id INT AUTO_INCREMENT PRIMARY KEY,
+    appointment_id INT PRIMARY KEY,
     customer_id INT,
     dog_id INT,
     service_id INT,
